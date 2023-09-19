f00262d59aeb873e29e59e376bbeedf81c4f10b3
javilak <javilak404@outlook.com>
2023-09-19 16:00:13 +0800

    new email signin logic

--- service/service.go
+++ service/service.go
@@ -5,6 +5,7 @@ import (
 	"github.com/gin-gonic/gin"
 	"go-im/helper"
 	"go-im/model"
+	"log"
 	"net/http"
 )
 
@@ -21,7 +22,7 @@ func Login(c *gin.Context) {
 		})
 		return
 	}
-	ub, err := model.GetUBbyAccountPw(account, helper.GetMd5(pwd))
+	ub, err := model.GetUBbyAccountPw(account, pwd)
 	if err != nil {
 		c.JSON(http.StatusOK, gin.H{
 			"code": -1,
@@ -43,5 +44,61 @@ func Login(c *gin.Context) {
 		"data": gin.H{
 			"token": token,
 		},
+	})
+}
+
+func UserDetail(c *gin.Context) {
+	u, _ := c.Get("user_claim")
+	fmt.Println(u)
+	Uc := u.(*helper.UserClaims)
+	ub, err := model.GetUBbyId(Uc.Identity)
+	if err != nil {
+		log.Printf("[DB ERR]:%v\n", err)
+		c.JSON(http.StatusOK, gin.H{
+			"code": -1,
+			"msg":  "数据查询异常",
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"code": 200,
+		"msg":  "数据加载成功",
+		"data": ub,
+	})
+
+}
+
+func SendCode(c *gin.Context) {
+	email := c.PostForm("email")
+	if email == "" {
+		c.JSON(http.StatusOK, gin.H{
+			"code": -1,
+			"msg":  "邮箱不能为空",
+		})
+		return
+	}
+	count, err := model.GetUBbyemail(email)
+	if err != nil {
+		fmt.Printf("[DB ERR: %v\n]", err)
+		return
+	}
+	if count > 0 {
+		c.JSON(http.StatusOK, gin.H{
+			"code": -1,
+			"msg":  "邮箱已被使用",
+		})
+		return
+	}
+	err = helper.SendCode(email, "666")
+	if err != nil {
+		c.JSON(http.StatusOK, gin.H{
+			"code": -1,
+			"msg":  "系统错误",
+		})
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"code": 200,
+		"msg":  "邮件已发送",
 	})
 }
