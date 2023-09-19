2b13621f2fd0ba73441288f0c91e45a3645dda77
javilak <javilak404@outlook.com>
2023-09-19 15:59:32 +0800

    new email check logic

--- model/user_basic.go
+++ model/user_basic.go
@@ -4,6 +4,7 @@ import (
 	"context"
 	"go-im/dao"
 	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 )
 
 type UserBasic struct {
@@ -27,4 +28,16 @@ func GetUBbyAccountPw(account, passwd string) (*UserBasic, error) {
 	err := dao.Mongo.Collection(UserBasic{}.CollectName()).FindOne(context.Background(), bson.D{{Key: "account", Value: account},
 		{Key: "password", Value: passwd}}).Decode(ub)
 	return ub, err
+}
+
+func GetUBbyId(id primitive.ObjectID) (*UserBasic, error) {
+	ub := &UserBasic{}
+	err := dao.Mongo.Collection(UserBasic{}.CollectName()).FindOne(context.Background(), bson.D{{Key: "_id", Value: id}}).Decode(ub)
+	return ub, err
+}
+
+func GetUBbyemail(email string) (int64, error) {
+	return dao.Mongo.Collection(UserBasic{}.CollectName()).
+		CountDocuments(context.Background(), bson.D{{Key: "email", Value: email}})
+
 }
