cd5a281c1a624fb8c5d88513ae68f4b0c25207d2
javilak <javilak404@outlook.com>
2023-09-19 16:00:37 +0800

    new email signin path

--- routers/router.go
+++ routers/router.go
@@ -2,11 +2,17 @@ package routers
 
 import (
 	"github.com/gin-gonic/gin"
+	"go-im/middlewares"
 	"go-im/service"
 )
 
 func Router() *gin.Engine {
 	r := gin.Default()
 	r.POST("/login", service.Login)
+	//验证码
+	r.POST("/send/code", service.SendCode)
+
+	auth := r.Group("/u", middlewares.Auth())
+	auth.GET("/user/detail", service.UserDetail)
 	return r
 }
